ca94122919466f4eab7b601935ed1cdf43244ec8
Jason Holutiak <jholutiak216@gmail.com>
2021-06-16 22:46:24 -0400

    Update RemoveOutofRangeKeys.py
    The script will now remove all keys if only out of range keys are present

--- RemoveOutofRangeKeys.py
+++ RemoveOutofRangeKeys.py
@@ -1,24 +1,24 @@
 from maya import cmds
 
+def setkeyframes(starttime, endtime):
+    cmds.currentTime(starttime, edit=True)
+    cmds.setKeyframe()
+    
+    cmds.currentTime(endtime, edit=True)
+    cmds.setKeyframe()
+
 def removeoutofrangekeys():
-    currenttime = cmds.currentTime(query=True)
     starttime = cmds.playbackOptions(minTime=True, query=True)
     endtime = cmds.playbackOptions(maxTime=True, query=True)
-    print(currenttime, starttime, endtime)
     
-    currenttime = cmds.currentTime(starttime, edit=True)
-    cmds.setKeyframe()
-    
-    currenttime = cmds.currentTime(endtime, edit=True)
-    cmds.setKeyframe()
-    
-    currenttime = cmds.currentTime(query=True)
     selection = cmds.ls(selection=True)
-    print(selection)
     
     for obj in selection:
-        print(obj)
+        keyframes = cmds.selectKey(obj, time=(starttime, endtime))
+        if keyframes != 0:
+            setkeyframes(starttime, endtime)
+            keyframes = cmds.selectKey(clear=True)
         cmds.cutKey(time=(starttime-1, starttime-1000))
-        cmds.cutKey(time=(endtime+1, endtime+1000))
-        
+        cmds.cutKey(time=(endtime+1, endtime+1000))    
+
 removeoutofrangekeys()
