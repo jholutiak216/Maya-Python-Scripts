0b6ffcd7adda942d42b515f3e350cb6d8fa8559e
Jason Holutiak <jholutiak216@gmail.com>
2021-06-19 01:17:01 -0400

    Update RemoveOutofRangeKeys.py
    Added clear flag to cutKey, should no longer pass the wrong values to attributes

--- RemoveOutofRangeKeys.py
+++ RemoveOutofRangeKeys.py
@@ -1,5 +1,8 @@
 from maya import cmds
 
+def printThis(this):
+    print(this)
+
 def setkeyframes(starttime, endtime):
     cmds.currentTime(starttime, edit=True)
     cmds.setKeyframe()
@@ -15,10 +18,11 @@ def removeoutofrangekeys():
     
     for obj in selection:
         keyframes = cmds.selectKey(obj, time=(starttime, endtime))
+        printThis(keyframes)
         if keyframes != 0:
             setkeyframes(starttime, endtime)
             keyframes = cmds.selectKey(clear=True)
-        cmds.cutKey(time=(starttime-1, starttime-1000))
-        cmds.cutKey(time=(endtime+1, endtime+1000))    
+        cmds.cutKey(time=(starttime-1, starttime-1000), clear=(True))
+        cmds.cutKey(time=(endtime+1, endtime+1000), clear=(True))  
 
 removeoutofrangekeys()
